af6c85727d36a686e6409471980d6e91e5021996
Potnuru-Sravanthi <sravanthipotnuru4@gmail.com>
2025-06-27 00:45:01 +0530

    Add files via upload

---
+++ mini _project 2.py
@@ -0,0 +1,11 @@
+# Age Calculator
+name = input("Enter your name: ")
+birth_year = int(input("Enter your birth year: "))
+current_year = int(input("Enter the current year: "))
+
+age = current_year - birth_year
+
+if age < 0:
+    print("Hmm... you haven't been born yet!")
+else:
+    print(f"Hello {name}, you are {age} years old.")
